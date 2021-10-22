31dff8ce60e66fe51e2245a7251efaf5d9ebe862
vishal-c-addweb <78903433+vishal-c-addweb@users.noreply.github.com>
2021-10-22 17:25:53 +0530

    Delete test directory

--- test/laravel-test/iMLogicalTest/frontend/README.md
+++
@@ -1,5 +0,0 @@
-### Installation: yarn
-
-### To run: yarn start
-
-### Change the environment variables to spin up the frontend instance and backend endpoint
--- test/laravel-test/iMLogicalTest/frontend/src/components/constants/frontEnd.js
+++
@@ -1,7 +0,0 @@
-export const LOGIN_API_URL='https://6de8-49-34-207-249.ngrok.io/api/login'
-export const AUTH_API = 'https://d097-49-34-207-249.ngrok.io/api/me'
-export const encrypt_key='iuj8i54k'
-export const intervalTime = 3
-export const success = "hello welcome you are successfully logged in"
-export const linkUsed = "link is already used"
-export const linkExpired = "link is expired"
